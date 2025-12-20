2112323d2a4c3c2815c23758c7020dc3228d0149
aakash <aakashprajapat2020@gmail.com>
2025-12-20 15:24:14 +0530

    testing  deployement aws s3  loacalstack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: 'export',
+  // trailingSlash: true,
+  // basePath: "/my-local-bucket",
+  // assetPrefix: "/my-local-bucket",
 };
 
 export default nextConfig;
